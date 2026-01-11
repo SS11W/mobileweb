bef752b51f391dd924c39c905b5a2b9101780076
Ekkasit-Ph <Ekkasit.ph@kkumail.com>
2026-01-11 16:57:58 +0700

    feat: add PWA toast component and status tap functionality
    - Implemented a new PWA toast component with customizable message and duration.
    - Added legacy support for the PWA toast component.
    - Introduced status tap functionality to enhance user experience on mobile devices.
    - Created swipe back gesture for improved navigation in the app.
    - Updated index.html to reference new asset files and ensure compatibility with legacy browsers.

--- apps/lab01_react/vite.config.ts
+++ apps/lab01_react/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mobileweb/lab01_react/',
+  base: './',
   plugins: [
     react(),
     legacy()
