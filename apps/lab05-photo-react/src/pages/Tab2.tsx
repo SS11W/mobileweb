73c70452d96237b239c128327b1759950cd24424
Ekkasit-Ph <Ekkasit.ph@kkumail.com>
2026-01-18 15:29:05 +0700

    feat: Add swipe back gesture and storage functionalities for Capacitor
    - Implemented swipe back gesture in `swipe-back-legacy-CAVubflp.js` for improved navigation experience.
    - Introduced `PreferencesWeb` class in `web-AV81ftr8.js` for managing local storage with methods for get, set, remove, and clear operations.
    - Added `FilesystemWeb` class in `web-BL-L1A0-.js` to handle file operations including read, write, delete, and directory management.
    - Created legacy support files for both gesture and storage functionalities to ensure compatibility with older versions.

---
+++ apps/lab05-photo-react/src/pages/Tab2.tsx
@@ -0,0 +1,43 @@
+import { camera } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/react';
+import './Tab2.css';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+
+const Tab2: React.FC = () => {
+  const { photos, addNewToGallery } = usePhotoGallery();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+          <IonTitle>Lab 05 - โดย เอกสิทธิ์ พินิจมนตรี รหัส 663380249-5</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+            <IonTitle size="large">Lab 05 - โดย เอกสิทธิ์ พินิจมนตรี รหัส 663380249-5</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => addNewToGallery()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
